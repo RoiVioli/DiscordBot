aefd38ea04a8ffdcee8651af5ad749f5fe0d61b9
RoiVioli <50718704+RoiVioli@users.noreply.github.com>
2025-02-19 11:35:03 +0100

    Update main.py
    fix

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class myclient(discord.Client):
     async def on_ready(self):
         print("Logged on as {0}".format(self.user))
 
-    async def on_message(self, message):  # Déplacez cette méthode à l'intérieur de la classe
+    async def on_message(self, message):  
         if message.author == self.user:
             return
         
